5a0d80d3d9f848896d6027e00679bd944e65413a
阿姨考勤 <deploy@local>
2026-07-28 17:52:04 +0800

    纯净单机版v2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { viteSingleFile } from 'vite-plugin-singlefile'
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: { '/api': { target: 'http://localhost:3000', changeOrigin: true } }
-  },
+  plugins: [vue(), viteSingleFile()],
   build: { outDir: 'dist', assetsDir: 'assets' }
 })
